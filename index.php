a02ebdd607dce1f48b014430a3434f2dbb014cb0
marcus gray <abundanceisfree@gmail.com>
2016-07-28 17:35:11 -0700

    add input form

--- index.php
+++ index.php
@@ -2,27 +2,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-  <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
-  <title>SHOUT IT</title>
-  <link rel="stylesheet" href="css/style.css" type ="text/css" />
-</head>
-<body>
-    <div id = "container">
-        <header>
-            <h1>SHOUT IT! SHOUTBOX</h1>
-        </header>
-        <div id="shouts">
-            <ul>
-                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
-                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
-                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
-                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
-                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+      <meta charset="utf-8">
+      <meta http-equiv="X-UA-Compatible" content="IE=edge">
+      <title>SHOUT IT</title>
+      <link rel="stylesheet" href="css/style.css" type ="text/css" />
+  </head>
+      <body>
+          <div id = "container">
+              <header>
+                  <h1>SHOUT IT! SHOUTBOX</h1>
+              </header>
+              <div id="shouts">
+                  <ul>
+                      <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                      <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                      <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                      <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                      <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
 
-            </ul>
-          </div>
-        </div>
-</body>
+                  </ul>
+                </div>
+              </div>
+              <div id="input">
+                  <form method="post" action="process.php">
+                      <input type="text" name="user" placeholder="Enter your Name">
+                      <input type="text" name="message" placeholder="Enter a message">
+                      <br />
+                      <input class="shout-btn" type="submit" name="submit" value="Shout It Out">
+              </div>
+      </body>
 </html>
 
