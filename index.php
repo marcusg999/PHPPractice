46e554eef9c33987e726415999a7550482e4a81e
marcus gray <abundanceisfree@gmail.com>
2016-07-28 17:29:25 -0700

    add styleing

--- index.php
+++ index.php
@@ -1,8 +1,28 @@
-<?php
-    function sayIt($words) {
-      echo $words;
-    }
-    sayIt('Hello Again');
-?>
 
+<!DOCTYPE html>
+<html>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <title>SHOUT IT</title>
+  <link rel="stylesheet" href="css/style.css" type ="text/css" />
+</head>
+<body>
+    <div id = "container">
+        <header>
+            <h1>SHOUT IT! SHOUTBOX</h1>
+        </header>
+        <div id="shouts">
+            <ul>
+                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+                <li class="shout"><span>10:15Pm-</span>Ogun: Hey what are you guys up to? </li>
+
+            </ul>
+          </div>
+        </div>
+</body>
+</html>
 
